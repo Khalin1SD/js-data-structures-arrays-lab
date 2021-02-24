b4eee8541577ecb6e8e47d8a1b95c0a0fae2c98a
Khalin Redding <khalinredd@gmail.com>
2021-02-23 22:04:45 -0500

    commit tree c2bfe7e365d5d8392bb8619a591f5425286a03f2

--- index.js
+++ index.js
@@ -1,1 +1,65 @@
 // Write your solution here!
+
+const drivers = ["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendDriver(name) {
+
+drivers.push(name)
+
+}
+
+function destructivelyPrependDriver(name) {
+
+drivers.unshift(name) 
+
+}
+
+function destructivelyRemoveLastDriver() {
+
+drivers.splice(-1,1) 
+
+}
+
+function destructivelyRemoveFirstDriver() {
+
+drivers.splice(0,1) 
+
+}
+
+function appendDriver(name) {
+
+const driversNew = drivers.slice()
+driversNew.push(name) 
+
+return driversNew
+
+}
+
+function prependDriver(name) {
+
+const driversNew = drivers.slice()
+driversNew.unshift(name) 
+return driversNew
+
+}
+
+function removeLastDriver(name) {
+
+const driversNew = drivers.slice()
+driversNew.splice(-1, 1) 
+
+return driversNew
+
+}
+
+function removeFirstDriver(name) {
+
+const driversNew = drivers.slice()
+driversNew.splice(0, 1) 
+
+return driversNew
+
+}
+
+
+
